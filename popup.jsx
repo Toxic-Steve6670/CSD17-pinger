ac2441566067d7cac4097cb7029d928c6a7e087f
Zhe Wang <akina.dwnhl@gmail.com>
2016-12-01 10:16:08 -0800

    added icons

--- popup.jsx
+++ popup.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './react/store/store';
-// import Root from './react/components/root';
+
+class Popup extends React.Component {
+  render(){
+    return(
+      <div>
+        Popup
+      </div>
+    );
+  }
+}
 
 document.addEventListener('DOMContentLoaded', ()=>{
-  // let store = configureStore();
-  // const root = document.getElementById('root');
-
-  ReactDOM.render(<Root store={store} />, root);
+  const root = document.getElementById('root');
+  ReactDOM.render(<Popup />, root);
 });
